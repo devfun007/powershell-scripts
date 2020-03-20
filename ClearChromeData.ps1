15c27f60b2ca9927587a6737ba669e52f6310e5d
devfun007 <50555792+devfun007@users.noreply.github.com>
2020-03-20 13:11:34 -0500

    Add files via upload

---
+++ ClearChromeData.ps1
@@ -0,0 +1,8 @@
+﻿Get-Process Chrome -ErrorAction SilentlyContinue | Stop-Process -Force
+
+$Items = @('User Data\*')
+            $users=GCI C:\Users\
+            Foreach($user in $users){
+$Folder = "C:\users\$($user)\AppData\local\Google\Chrome"
+$Items | ForEach-Object { Remove-Item "$Folder\$_" -force -Recurse}
+}

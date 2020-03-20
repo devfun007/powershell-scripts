6453a84a5431e426f562266d2fe7d01e1be20279
devfun007 <50555792+devfun007@users.noreply.github.com>
2020-03-20 16:04:51 -0500

    Add files via upload

---
+++ cleanPCobjects.ps1
@@ -0,0 +1,9 @@
+﻿import-module activedirectory
+
+$45 = (Get-Date).AddDays(-45)
+$60 = (Get-Date).AddDays(-60) 
+ 
+ 
+Get-ADComputer -Filter 'lastLogonDate -lt $45 -and OperatingSystem -NotLike "*Server*"' | Move-ADObject -TargetPath "OU=___,OU=___,DC=__,DC=__" 
+ 
+Get-ADComputer -SearchBase "OU=___,OU=___,DC=__,DC=__" -Filter 'lastLogonDate -lt $60 -and OperatingSystem -NotLike "*Server*"' | Remove-ADObject -Recursive -Confirm:$false

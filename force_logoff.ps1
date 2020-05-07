e953d7d582e963f824de4cc2b4c16e4b59fab1b2
Mohamed <50555792+devfun007@users.noreply.github.com>
2020-05-07 13:34:06 -0500

    Rename force_logoff.ps1 to force_logoff_remotely.ps1

--- force_logoff.ps1
+++
@@ -1,3 +0,0 @@
-﻿$users = Get-ADUser -filter * -SearchBase "ou=,dc=,dc="
-
-Get-RDUserSession | where { $users.sAMAccountName -contains $_. } | % { $_ | Invoke-RDUserLogoff -Force }
---
+++ force_logoff_remotely.ps1
@@ -0,0 +1,3 @@
+﻿$users = Get-ADUser -filter * -SearchBase "ou=,dc=,dc="
+
+Get-RDUserSession | where { $users.sAMAccountName -contains $_. } | % { $_ | Invoke-RDUserLogoff -Force }
